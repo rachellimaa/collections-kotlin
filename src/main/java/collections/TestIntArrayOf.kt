949983040e073e447b640436a4005a1b774dce38
Rachel Lima <rachel.l@samsung.com>
2021-02-15 18:46:28 -0400

    Add arrayOf in kotlin

---
+++ src/main/java/collections/TestIntArrayOf.kt
@@ -0,0 +1,16 @@
+package collections
+
+fun main(){
+    val values = intArrayOf(2,3,1,10,7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("""""""""""""""""""")
+
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+}
