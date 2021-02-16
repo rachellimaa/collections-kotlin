124c0c1c50977cfe22dd030548cf8fd6dc5695ef
Rachel Lima <rachel.l@samsung.com>
2021-02-15 20:00:05 -0400

    Add test map

---
+++ src/main/java/collections/TestMap.kt
@@ -0,0 +1,18 @@
+package collections
+
+fun main() {
+    val pair: Pair<String, Double> = Pair("Joao", 1000.0)
+    val map1 = mapOf(pair)
+
+    map1.forEach { (t, u) ->
+        println("Chave: $t - Valor: $u")
+    }
+
+    val map2 = mapOf(
+            "Pedro" to 2500.0,
+            "Maria" to 1500.0)
+
+    map2.forEach { (t, u) ->
+        println("Chave: $t - Valor: $u")
+    }
+}
